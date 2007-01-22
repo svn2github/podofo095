ea6d870f8f8c11772b5aefbd85e80ed7655a0991
ringerc <ringerc@e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5>
2007-01-22 04:05:04 +0000

    Improved FreeType detector
    git-svn-id: svn://svn.code.sf.net/p/podofo/code@346 e6e4d1b7-ffb3-4f57-9d89-6d52b3892af5

--- podofobrowser/qt4port/cmake/modules/FindLIBFREETYPE.cmake
+++ podofobrowser/qt4port/cmake/modules/FindLIBFREETYPE.cmake
@@ -3,21 +3,26 @@
 # where freetype.h is relative to it.
 # TODO: use pkg-config and/or freetype-config to find freetype when possible
 FIND_PATH(LIBFREETYPE_FT2BUILD_H NAMES ft2build.h)
-MESSAGE("Found ft2build.h in ${LIBFREETYPE_FT2BUILD_H}")
-# Now try to find the corresponding freetype.h
-FIND_PATH(LIBFREETYPE_FREETYPE_H
-    NAMES freetype/freetype.h
+# We don't look for freetype.h since a file of the same name appears in
+# FreeType 1 and confuses the detection code. Instead, locate
+# config/ftheader.h since this does NOT appear in freetype 1.
+# This file may appear in:
+#	freetype2/freetype/config
+#	freetype/freetype/config
+#	freetype2/config
+# depending on the freetype2 packaging and version.
+FIND_PATH(LIBFREETYPE_FTHEADER_H
+    NAMES config/ftheader.h freetype/config/ftheader.h
     PATHS
     ${LIBFREETYPE_FT2BUILD_H}/freetype2
     ${LIBFREETYPE_FT2BUILD_H}/freetype
     )
-MESSAGE("Found freetype.h in ${LIBFREETYPE_FREETYPE_H}")
 # At least the library will be somewhere sensible
 FIND_LIBRARY(LIBFREETYPE_LIB NAMES freetype libfreetype)
-MESSAGE("Found freetype library at ${LIBFREETYPE_LIB}")
+MESSAGE("freetype lib: ${LIBFREETYPE_LIB}")
 
-IF(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FREETYPE_H AND LIBFREETYPE_LIB)
+IF(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FTHEADER_H AND LIBFREETYPE_LIB)
 	SET(LIBFREETYPE_FOUND TRUE CACHE BOOLEAN "Was libfreetype found")
-ELSE(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FREETYPE_H AND LIBFREETYPE_LIB)
+ELSE(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FTHEADER_H AND LIBFREETYPE_LIB)
 	SET(LIBFREETYPE_FOUND FALSE CACHE BOOLEAN "Was libfreetype found")
-ENDIF(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FREETYPE_H AND LIBFREETYPE_LIB)
+ENDIF(LIBFREETYPE_FT2BUILD_H AND LIBFREETYPE_FTHEADER_H AND LIBFREETYPE_LIB)
